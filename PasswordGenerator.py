79ea00c7cf064b4f2f69e4cd7985d79ad7353423
Niranjan Bhol <niranjan.bhol@icloud.com>
2024-08-28 16:31:04 +0530

    Version 2
    Version 1.1 - Improvement in version 1

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,14 +1,22 @@
 import random
 import string
 
-# Get the length of the password from the user
-length = int(input("Enter the length of the password: "))
+def generate_password(length):
+    # Define the character set: lowercase, uppercase, digits, and symbols
+    characters = string.ascii_letters + string.digits + string.punctuation
+    
+    # Generate the password
+    password = ''.join(random.choice(characters) for i in range(length))
+    
+    return password
 
-# Define the character set
-characters = string.ascii_letters + string.digits + string.punctuation
-
-# Generate the password
-password = ''.join(random.choice(characters) for i in range(length))
-
-# Printing the password
-print("Generated password:", password)
+# Get the desired length of the password from the user
+try:
+    length = int(input("Enter the length of the password: "))
+    if length <= 0:
+        print("Password length must be a positive integer.")
+    else:
+        password = generate_password(length)
+        print("Generated password:", password)
+except ValueError:
+    print("Please enter a valid integer for the password length.")
