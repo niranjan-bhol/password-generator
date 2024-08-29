0ca6e4f1d4c70ad04914ceaa77accb9e4eab574a
Niranjan Bhol <niranjan.bhol@icloud.com>
2024-08-29 12:10:00 +0530

    Version 3
    Version 2.0 - Change in algorithm

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,22 +1,49 @@
-import random
 import string
+import random
 
-def generate_password(length):
-    # Define the character set: lowercase, uppercase, digits, and symbols
-    characters = string.ascii_letters + string.digits + string.punctuation
-    
-    # Generate the password
-    password = ''.join(random.choice(characters) for i in range(length))
-    
-    return password
+# Getting password length
+length = int(input("Enter password length: "))
 
-# Get the desired length of the password from the user
-try:
-    length = int(input("Enter the length of the password: "))
-    if length <= 0:
-        print("Password length must be a positive integer.")
+print('''Choose character set for password from below : 
+      1. Letters
+      2. Digits
+      3. Special Characters
+      4. Exit''')
+
+characterList = ""
+
+# Getiing character set for password
+while(True):
+    
+    choice = int(input("Enter your choice: "))
+    
+    if(choice == 1):
+        # Adding letters to possible characters
+        characterList += string.ascii_letters
+        
+    elif(choice == 2):
+        # Adding digits to possible characters
+        characterList += string.digits
+        
+    elif(choice == 3):
+        # Adding special characters to possible characters
+        characterList += string.punctuation
+        
+    elif(choice == 4):
+        break
+    
     else:
-        password = generate_password(length)
-        print("Generated password:", password)
-except ValueError:
-    print("Please enter a valid integer for the password length.")
+        print("Please enter a valid option")
+
+password = []
+
+for i in range(length):
+
+    # Picking a random character from characterList
+    random_char = random.choice(characterList)
+
+    # Appending a random character to password
+    password.append(random_char)
+
+# printing password as a string
+print("The generated random password is " + ''.join(password))
